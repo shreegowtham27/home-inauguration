23663822f5526c81ea6cb5abb72f6fa86a39da58
Shree Gowtham Selvam <shreegowtham.selvam@nbcuni.com>
2024-11-19 12:35:30 +0530

    Updated title

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import Hero from "./components/hero";
 import NavBar from "./components/NavBar";
-import VenueDetails from "./venue";
 import MapsComponent from "./components/maps"
 import Venue from "./components/venue";
 import ContactForm from "./components/contact"
