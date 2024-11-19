8e2d96d206138354cd23726743eff5baa0317927
Shree Gowtham Selvam <shreegowtham.selvam@nbcuni.com>
2024-11-19 14:40:08 +0530

    removed grid for responsiveness

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -6,7 +6,7 @@ export default function Hero(){
     return(
         <div id="#" className="hero min-h-screen">
             {/* //bg-gradient-to-r from-emerald-500 to-emerald-900 */}
-            <div className="hero-content text-center grid">
+            <div className="hero-content text-center">
                 <div className="container text-4xl">
                 <p className="py-6 font-extrabold py-3 font-drSugi bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">
                     You&apos;re Cordially Invited for the <br/> <br/> <b className="p-1 px-8 font-drSugi py-3 p-4 bg-gradient-to-r from-emerald-500 to-emerald-900 bg-clip-text">House Warming Ceremony  of our <span className="highlight-text text-emerald-800 px-4">Very First Home</span> of upcoming Many...</b>
